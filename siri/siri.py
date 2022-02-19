4012c976d645737c533444b7b29bc9004ea0c988
ɢx  ₪ᴇᴏ <89633118+GxNeo@users.noreply.github.com>
2022-02-19 14:43:38 +0530

    Update siri.py

--- siri/siri.py
+++ siri/siri.py
@@ -26,6 +26,7 @@ async def start(client, message):
     n=await m.edit("..")
     o=await n.edit("...")
     await o.edit(text=Config.START_MSG.format(message.from_user.mention),
+         disable_web_page_preview=True,
          reply_markup=InlineKeyboardMarkup(
             [
                 [
